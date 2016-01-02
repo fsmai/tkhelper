85a787824abbe34ec7d6f10371c3fcc14e9f65c4
fsmai <farid.smai@gmail.com>
2016-01-02 13:19:25 +0100

    mise en forme

--- graphcalc.py
+++ graphcalc.py
@@ -1,35 +1,24 @@
 """ un exemple de gui avec tkinter
 
 A noter:
-1) on peut structurer le code [au lieu d'un script avec tout au meme niveau]
+1) on peut structurer le code :
+    pas un script avec tout au meme niveau
+    pas de variables globales
 2) utiliser grid() plutot que pack()
 """
+
+
 import numpy
 from tkhelper import tk, ttk, Plot, command
 
 
-@command
-def refresh_plot(plot, xexpr, yexpr):
-    ## traite les infos
-    globals_ = dict(vars(numpy))
-    exec('__x = %s' % xexpr.get(), globals_)
-    exec('__y = lambda x: %s' % yexpr.get(), globals_)
-    x = numpy.array(globals_['__x'])
-    fy = globals_['__y']
-    y = fy(x)
-
-    ## maj du plot
-    a = plot.figure.add_subplot(111)
-    a.plot(x, y, label=yexpr.get())
-    a.legend(loc='best')
-    plot.canvas.draw()  # imperatif sinon rien ne se passe
-
-
-@command
-def clear_plot(plot):
-    a = plot.figure.add_subplot(111)
-    a.clear()
-    plot.canvas.draw()  # imperatif sinon rien ne se passe
+def main():
+    # le 'toplevel widget' -> la fenetre de l'appli
+    root = tk.Tk()
+    # on habille la fenetre
+    build_graphcalc(root)
+    # on lance le tout
+    root.mainloop()
 
 
 def build_graphcalc(root):
@@ -79,10 +68,29 @@ def build_graphcalc(root):
     content.rowconfigure(2, weight=1)
 
 
+@command
+def refresh_plot(plot, xexpr, yexpr):
+    ## traite les infos
+    globals_ = dict(vars(numpy))
+    exec('__x = %s' % xexpr.get(), globals_)
+    exec('__y = lambda x: %s' % yexpr.get(), globals_)
+    x = numpy.array(globals_['__x'])
+    fy = globals_['__y']
+    y = fy(x)
+
+    ## maj du plot
+    a = plot.figure.add_subplot(111)
+    a.plot(x, y, label=yexpr.get())
+    a.legend(loc='best')
+    plot.canvas.draw()  # imperatif sinon rien ne se passe
+
+
+@command
+def clear_plot(plot):
+    a = plot.figure.add_subplot(111)
+    a.clear()
+    plot.canvas.draw()  # imperatif sinon rien ne se passe
+
+
 if __name__ == '__main__':
-    # le 'toplevel widget' -> la fenetre de l'appli
-    root = tk.Tk()
-    # on habille la fenetre
-    build_graphcalc(root)
-    # on lance le tout
-    root.mainloop()
+    main()
